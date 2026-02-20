0b5bdcc8bd3f346938e32983a299a5624a1c872f
valeria <158862303+Vallellia@users.noreply.github.com>
2026-02-20 15:00:46 -0800

    Update sesion_03.py

--- sesion_03.py
+++ sesion_03.py
@@ -1,51 +1,59 @@
-# loop
-mi_list = [1,2,3,4,5]
-for i in mi_list:
-  print("El numero es: ", i)
-  resul = 0
-  for i in mi_list:
-    resul += i
-    print(f"El resultado de la suma de mi_lista es: (resul) ")
-    for i in range(2,9) : 
-      print(i)
-      mi_list_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
-      for i in mi_list_2:
-        if i != "lunes":
-          print(f"feliz(i)!")
+#loops
+mi_li = [1,2,3,4,5]
+
+for i in mi_li:
+    print("El numero es: ", i)
+
+    resul = 0
+    for i in mi_li:
+        resul += i
+print(f"El resultado de la suma de mi lista es: {resul} ")
+
+for i in range(2,9):
+            print(i)
+
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+for i in mi_lista_2:
+       if i != "Lunes":
+              print(f"Feliz{i}!")
+
 #while loop
-i = 0
+i=0
+
 while i < 5:
-  i +=1
-  if i==3:
-    continue
-    print(i)
-    if i==4:
-      break
-    else:
-      print("i es ahora mayir o igual a 5")
-# practica 2
-# dada la lista mi_lista = [lunes, jueves, miercoles, jueves, viernes]
-# imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
-# pista: usa los dos tipos de loops while y for en el mismo programa para lograrlo
+       i +=1
+       if i==3:
+              continue
+       print(i)
+       if i ==4:
+              break
+else:
+       print("i es ahora mayor o igual a 5")
+
+#Practica 2
+# Dada la lista mi_lista_2 = ["Lunes", "Jueves", "Miercoles", "Jueves", "Viernes"]
+# Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# Pista: Usa  los dos tipos de loops while y for en el mismo programa para lograrlo
 # resultado:
 # martes
 # miercoles
-# jueves
-# viernes
+# jueves 
+# viernes 
 # martes
 # miercoles
-# jueves
-# viernes
+# jueves 
+# viernes 
 # martes
-#miercoles
-#jueves
-#viernes
+# miercoles
+# jueves 
+# viernes 
 
 i = 0
-mi_lista_2 = ["lunes", "martes", "miercoles", "miercoles", "jueves", "viernes"]
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 while i< 3:
-  i += 1
-
-for d in mi_lista_2:
-  if d != "lunes"
-  print(d)
+    i += 1
+ 
+    for d in mi_lista_2:
+        if d != "Lunes":
+           print(d)
