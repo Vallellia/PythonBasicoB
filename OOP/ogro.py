658325d71303da904d2a57cc87b98db0a2bd17e1
vallellia <aslivaleria197@gmail.com>
2026-02-27 15:41:14 -0800

    programacion orientada a objetos Enemigo.py

--- OOP/ogro.py
+++ OOP/ogro.py
@@ -1,7 +1,16 @@
-from enemigo import *
-class ogro(enemigo):
-    def __init__(self, puntos_energia=20, ataque=3):
-        super().__init__(tipo_enemigo='ogro', puntos_energia=puntos_energia, ataque=ataque)
-        
+from Enemigo import *
+import random
+
+class ogro(Enemigo):
+    def __init__(self, punto_energia=20, ataque=3):
+        super().__init__(tipo_enemigo='Ogro', punto_energia=punto_energia, ataque=ataque)
+
     def habla(self):
-        print("ogro aplasta todo!!!")
+        print("Ogro aplasta todo!!!")
+
+def ataque_especial(self):
+    print("ogro ataque especial")
+    funciona_ataque_especial = random.random() <0.20
+    if funciona_ataque_especial:
+        self.ataque += 4
+        print("ogro enojado y incremento su ataque por 4!!!")
--- OOP/zombie.py
+++ OOP/zombie.py
@@ -1,11 +1,20 @@
-from enemigo import *
+from Enemigo import *
+import random
 
-class zombie(enemigo):
-    def __int__(self, puntos_energia=10, ataque=1):
-        super().__init__(tipo_enemigo='zombie', puntos_energia=puntos_energia, ataque=ataque)
+
+class Zombie(Enemigo):
+    def __init__(self, punto_energia=10, ataque=1):
+        super().__init__(tipo_enemigo='Zombie', punto_energia=punto_energia, ataque=ataque)
 
     def habla(self):
-        print("Hummm....*")
+        print("Hummmm......")
 
     def propagar_enfermedad(self):
-        print("el zombie esta tratando de propagar la enfermedad!!")
+        print("El xombie esta tratando de propagar la enfermedad!!")
+
+def ataque_especial(self):
+    print("ogro ataque especial")
+    funciona_ataque_especial = random.random() < 0.50
+    if funciona_ataque_especial:
+        self.ataque += 2
+        print("zombie ha regenerado su energia con 2HP!!!!")
